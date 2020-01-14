b799a21cbb5e53a5576b342eb438245cc2bc8341
Zarko Dencic <denciczarko@gmail.com>
2020-01-14 16:31:19 +0100

    lesson #3

--- src/UseTransition/useTransition.tsx
+++ src/UseTransition/useTransition.tsx
@@ -3,8 +3,8 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
 import {
   bInterpolate,
-  transformOrigin,
-  useTransition
+  // transformOrigin,
+  useTimingTransition
 } from "react-native-redash";
 
 import { Button, Card, StyleGuide, cards } from "../components";
@@ -15,20 +15,62 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-end"
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: StyleGuide.spacing * 4
   }
 });
 
+const transformOrigin = -(width / 2 - StyleGuide.spacing * 2);
+
 export default () => {
+  const [toggled, setToggled] = useState(false);
+  const transitionVal = useTimingTransition(toggled, {
+    duration: 400,
+    easing: Easing.inOut(Easing.ease)
+  });
   return (
     <View style={styles.container}>
       {cards.map((card, index) => {
+        const rotation = interpolate(index, {
+          inputRange: [0, 1, 2],
+          outputRange: [-1, 0, 1]
+        });
+        const rotate = multiply(
+          rotation,
+          interpolate(transitionVal, {
+            inputRange: [0, 1],
+            outputRange: [0, Math.PI / 6]
+          })
+        );
         return (
-          <Animated.View key={card.id}>
+          <Animated.View
+            key={card.id}
+            style={[
+              styles.overlay,
+              {
+                transform: [
+                  { translateX: transformOrigin },
+                  {
+                    rotate
+                  },
+                  { translateX: -transformOrigin }
+                ]
+              }
+            ]}
+          >
             <Card {...{ card }} />
           </Animated.View>
         );
       })}
-      <Button label="Start" primary onPress={() => true} />
+      <Button
+        label={toggled ? "Reset" : "Start"}
+        primary
+        onPress={() => setToggled(prev => !prev)}
+      />
     </View>
   );
 };
