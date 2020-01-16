fdcac85b3b83fb602a935e048556b3855c256214
Zarko Dencic <denciczarko@gmail.com>
2020-01-16 20:35:28 +0100

    lesson #4

--- src/DarkMode/DarkMode.tsx
+++ src/DarkMode/DarkMode.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from "react";
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 import {
   Transition,
@@ -15,10 +15,20 @@ import Followers from "./Followers";
 
 export { profilePic } from "./ProfilePic";
 
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={400} />
+    <Transition.Out type="fade" durationMs={500} />
+  </Transition.Together>
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between"
+  },
+  darkMask: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "black"
   },
   text: {
     textAlign: "center"
@@ -26,24 +36,35 @@ const styles = StyleSheet.create({
 });
 
 export default () => {
+  const [dark, setDark] = useState(false);
+  const ref = useRef<TransitioningView>(null);
   return (
-    <View style={styles.container}>
-      <SafeAreaView />
+    <Transitioning.View style={styles.container} {...{ ref, transition }}>
+      {dark && <View style={styles.darkMask} />}
+      <Switch
+        value={dark}
+        onValueChange={value => {
+          if (ref.current) {
+            ref.current.animateNextTransition();
+          }
+          setDark(value);
+        }}
+      />
       <ProfilePic />
       <View>
-        <Text type="title3" style={styles.text}>
+        <Text type="title3" style={styles.text} {...{ dark }}>
           Krzysztof Magiera
         </Text>
-        <Text type="headline" style={styles.text}>
+        <Text type="headline" style={styles.text} {...{ dark }}>
           Kraków, Poland
         </Text>
       </View>
-      <Followers followers={3569} following={310} />
+      <Followers followers={3569} following={310} {...{ dark }} />
       <SocialMediaIcons />
-      <Text type="body" style={styles.text}>
+      <Text type="body" style={styles.text} {...{ dark }}>
         When speaking of animations, the key to success is to avoid frame drops
       </Text>
-      <Button label="Follow" primary onPress={() => {}} />
-    </View>
+      <Button label="Follow" primary onPress={() => true} />
+    </Transitioning.View>
   );
 };
