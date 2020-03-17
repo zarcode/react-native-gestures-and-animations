cc4e9739ea89adecc346378b8d82144c1dc36c79
Zarko Dencic <denciczarko@gmail.com>
2020-03-17 20:07:00 +0100

    lesson swipe

--- src/Swipe/Profiles.tsx
+++ src/Swipe/Profiles.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Dimensions, SafeAreaView, StyleSheet } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
 import Animated from "react-native-reanimated";
 import { Feather as Icon } from "@expo/vector-icons";
 
@@ -7,8 +7,22 @@ import { useMemoOne } from "use-memo-one";
 import { RectButton } from "react-native-gesture-handler";
 import Card, { Profile } from "./Profile";
 import Swipeable from "./Swipeable";
-import { timing, StyleGuide } from "../components";
+import { StyleGuide, timing } from "../components";
 
+const {
+  Value,
+  Clock,
+  interpolate,
+  concat,
+  Extrapolate,
+  block,
+  cond,
+  set,
+  useCode,
+  not,
+  clockRunning,
+  call
+} = Animated;
 const { width, height } = Dimensions.get("window");
 const deltaX = width / 2;
 const α = Math.PI / 12;
@@ -57,8 +71,73 @@ interface ProfilesProps {
 
 export default ({ profiles }: ProfilesProps) => {
   const [index, setIndex] = useState(0);
+  const { x, y, offsetX, like, dislike } = useMemoOne(
+    () => ({
+      x: new Value(0),
+      y: new Value(0),
+      offsetX: new Value(0),
+      like: new Value(0) as Animated.Value<0 | 1>,
+      dislike: new Value(0) as Animated.Value<0 | 1>
+    }),
+    []
+  );
+  const onSnap = useMemoOne(
+    () => ([point]: readonly number[]) => {
+      if (point !== 0) {
+        offsetX.setValue(0);
+        setIndex((index + 1) % profiles.length);
+      }
+    },
+    [index, offsetX, profiles.length]
+  );
   const profile = profiles[index];
-
+  const rotateZ = concat(
+    interpolate(x, {
+      inputRange: [-1 * deltaX, deltaX],
+      outputRange: [α, -1 * α],
+      extrapolate: Extrapolate.CLAMP
+    }),
+    "rad"
+  );
+  const likeOpacity = interpolate(x, {
+    inputRange: [0, deltaX / 4],
+    outputRange: [0, 1]
+  });
+  const nopeOpacity = interpolate(x, {
+    inputRange: [(-1 * deltaX) / 4, 0],
+    outputRange: [1, 0]
+  });
+  const translateX = x;
+  const translateY = y;
+  const clock = new Clock();
+  useCode(
+    () =>
+      block([
+        cond(dislike, [
+          set(
+            offsetX,
+            timing({
+              clock,
+              from: offsetX,
+              to: -A
+            })
+          ),
+          cond(not(clockRunning(clock)), [set(dislike, 0), call([], onSnap)])
+        ]),
+        cond(like, [
+          set(
+            offsetX,
+            timing({
+              clock,
+              from: offsetX,
+              to: A
+            })
+          ),
+          cond(not(clockRunning(clock)), [set(like, 0), call([], onSnap)])
+        ])
+      ]),
+    [dislike, offsetX, clock, onSnap, like]
+  );
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -68,23 +147,19 @@ export default ({ profiles }: ProfilesProps) => {
       <View style={styles.cards}>
         <Animated.View
           style={{
-            ...StyleSheet.absoluteFillObject
+            ...StyleSheet.absoluteFillObject,
+            transform: [{ translateX }, { translateY }, { rotateZ }]
           }}
         >
-          <Card {...{ profile }} />
+          <Card {...{ profile, likeOpacity, nopeOpacity }} />
         </Animated.View>
+        <Swipeable key={index} {...{ snapPoints, onSnap, x, y, offsetX }} />
       </View>
       <View style={styles.footer}>
-        <RectButton
-          style={styles.circle}
-          onPress={() => setIndex((index + 1) % profiles.length)}
-        >
+        <RectButton style={styles.circle} onPress={() => dislike.setValue(1)}>
           <Icon name="x" size={32} color="#ec5288" />
         </RectButton>
-        <RectButton
-          style={styles.circle}
-          onPress={() => setIndex((index + 1) % profiles.length)}
-        >
+        <RectButton style={styles.circle} onPress={() => like.setValue(1)}>
           <Icon name="heart" size={32} color="#6ee3b4" />
         </RectButton>
       </View>
