a28978cb0561cd4c586d1ec124af179c0a8f57dd
Zarko Dencic <denciczarko@gmail.com>
2020-05-21 22:17:27 +0200

    cleaning

--- src/Switcher/Amplify.tsx
+++ src/Switcher/Amplify.tsx
@@ -4,19 +4,9 @@ import { PanGestureHandler, State } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
 import { useMemoOne } from "use-memo-one";
 import { onGestureEvent } from "react-native-redash";
+import diffClamp from "./diffClamp";
 
-const {
-  Value,
-  diffClamp,
-  cond,
-  set,
-  eq,
-  add,
-  sub,
-  debug,
-  defined,
-  not,
-} = Animated;
+const { Value, cond, set, eq, add, sub } = Animated;
 const { block, useCode, call } = Animated;
 
 const INITHEIGHT = 300;
@@ -31,44 +21,18 @@ const withOffset = (
   const safeOffset: Animated.Value<number> = new Value(-1);
 
   return block([
-    debug("safeOffset", safeOffset),
     cond(
       eq(safeOffset, -1), // ako nije definisan
-      [
-        debug("postavi", offset),
-        set(safeOffset, offset === undefined ? 0 : offset),
-      ] // postavi na 0 ili ofset iz args
+      [set(safeOffset, offset === undefined ? 0 : offset)] // postavi na 0 ili ofset iz args
     ),
-    // cond(
-    //   eq(gestureState, State.END),
-    //   [set(offset, add(offset, value)), offset],
-    //   [add(offset, value)]
-    // );
 
     cond(
       eq(gestureState, State.ACTIVE),
-      [debug("active", value), add(safeOffset, value)],
-      [
-        // debug("sacuvaj state", gestureState),
-        // debug("sacuvaj value", value),
-        debug("sacuvaj of", value),
-        // ako je aktivna animacija stavi vrednost da bude ofset
-        set(safeOffset, add(safeOffset, value)), // kad se zavrsi na safe ofset dodaj value
-      ]
+      [add(safeOffset, value)],
+      [set(safeOffset, add(safeOffset, value))]
     ),
   ]);
 };
-
-// const withOffset = (
-//   value: Animated.Node<number>,
-//   state: Animated.Value<State>,
-//   offset: Animated.Value<number> = new Value(0)
-// ) =>
-//   cond(
-//     eq(state, State.END),
-//     [set(offset, add(offset, value)), offset],
-//     [add(offset, value)]
-//   );
 
 interface AmplifyProps {
   height: number;
@@ -88,7 +52,6 @@ const Amplify = ({
 }: AmplifyProps) => {
   const initialTranslationY = ((100 - initialValue) * itemsHeight) / 100;
 
-  // const state = new Value(State.UNDETERMINED);
   const translationY = new Value(0);
 
   const { state } = useMemoOne(() => {
@@ -111,8 +74,6 @@ const Amplify = ({
 
     const newValue = (100 * (itemsHeight - x)) / itemsHeight;
 
-    // console.log("newValue", newValue);
-    // console.log("initialValue", initialValue);
     handler = setTimeout(() => {
       if (newValue !== initialValue) {
         onChange(newValue);
@@ -120,26 +81,13 @@ const Amplify = ({
     }, 400);
   };
 
-  // v = 300x/100
-
-  const tY = withOffset(translationY, state, new Value(initialTranslationY));
-
-  const translateY = tY;
-  // const translateY = diffClamp(tY, 0, itemsHeight);
-  // const translateY = tY;
-
-  // const test = new Value();
-  // const test = new Value(undefined);
-  useCode(
-    () =>
-      block([
-        // debug("tY", tY),
-        // debug("translateY", translateY),
-        // debug("offsetY", offsetY),
-        call([translateY], onSnap),
-      ]),
-    [translateY]
+  const translateY = diffClamp(
+    withOffset(translationY, state, new Value(initialTranslationY)),
+    0,
+    INITHEIGHT
   );
+
+  useCode(() => block([call([translateY], onSnap)]), [translateY]);
 
   return (
     <PanGestureHandler {...gestureHandler} key={initialValue}>
---
+++ src/Switcher/diffClamp.tsx
@@ -0,0 +1,15 @@
+import Animated from "react-native-reanimated";
+
+const { cond, diff, set, add, proc, eq, min, max, Value } = Animated;
+
+const procAcc = proc(function(a, minVal, maxVal, value) {
+  return set(
+    value,
+    min(max(add(cond(eq(value, -1), value, a), diff(a)), minVal), maxVal)
+  );
+});
+
+export default function diffClamp(a, minVal, maxVal) {
+  const value = new Value(-1);
+  return procAcc(a, minVal, maxVal, value);
+}
